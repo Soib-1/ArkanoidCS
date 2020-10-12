138373c83ac04bf996f3255c6138e9e1a0496882
Soib <lukasz_cybulski@icloud.com>
2020-10-12 21:01:25 +0200

    Initial commit

---
+++ Arkanoid/Block.cs
@@ -0,0 +1,7 @@
+﻿namespace Arkanoid
+{
+    public class Block
+    {
+        
+    }
+}
---
+++ Arkanoid/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Arkanoid
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
